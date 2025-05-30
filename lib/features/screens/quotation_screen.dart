b5831d14bb20f69b3704b21a9d3e547e4d3e7f73
vara-prasad-f9 <ux@f9tech.com>
2025-05-30 18:25:47 +0530

    ignore styles

--- lib/features/screens/quotation_screen.dart
+++ lib/features/screens/quotation_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:steel_budy/services/api_service.dart';
 import 'package:steel_budy/features/screens/quotation_details.dart';
